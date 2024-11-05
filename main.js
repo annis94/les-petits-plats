3fbca0e98cf120c7124dd36f802471bdd6054227
annis94 <anis561@gmail.com>
2024-11-05 01:02:24 +0100

    correction

--- main.js
+++ main.js
@@ -106,12 +106,14 @@ document.addEventListener('DOMContentLoaded', () => {
                     <span class="transform transition-transform duration-200">&darr;</span>
                 </button>
                 <div class="dropdown-content hidden absolute mt-2 w-64 bg-white rounded-lg shadow-lg z-50">
-                    <div class="p-2 border-b">
+                   <div class="p-2 border-b">
                         <div class="relative">
-                            <input type="text" 
-                                placeholder="Rechercher un ${filter.label.toLowerCase()}"
-                                class="w-full p-2 pr-8 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-400">
-                            <button class="clear-search hidden absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700">
+                            <input 
+                                type="text" 
+                                class="w-full p-2 pr-10 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                            />
+                            <i class="fa-solid fa-magnifying-glass absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"></i>
+                            <button class="clear-search hidden absolute right-8 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700">
                                 &times;
                             </button>
                         </div>
